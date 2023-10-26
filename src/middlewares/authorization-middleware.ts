7a2ca1d7c61d60b6e520399ae1066f4c3d4afc07
VlaDinter <vladyslavdinter@gmail.com>
2023-10-26 18:00:41 +0300

    Fix authorization middleware

--- src/middlewares/authorization-middleware.ts
+++ src/middlewares/authorization-middleware.ts
@@ -2,8 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 import { CodeResponsesEnum } from '../types';
 
 export const authorizationMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const encodedAdmin = Buffer.from('admin:qwerty').toString('base64');
-    const isAuthorized = req.headers.authorization?.endsWith(encodedAdmin);
+    const authorization = `Basic ${Buffer.from('admin:qwerty').toString('base64')}`;
+    const isAuthorized = req.headers.authorization === authorization;
 
     if (!isAuthorized) {
         res.send(CodeResponsesEnum.Unauthorized_401);
